7ea1229afc30f06f24f59e39797874d6de8ce71e
christophe bellec <christophe.bellec@ekim.fr>
2021-12-17 15:52:37 +0100

    responsive

---
+++ src/pages/QualiteEauPotable/Resultats/Cartes.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { convertDate } from "../../../utils";
+import Loading from "../../../common/Loading";
+import { Resultat } from "../types";
+
+type CartesProps = {
+  resultats: Resultat[];
+  resultIsLoading: boolean;
+};
+export default function Cartes(props: CartesProps) {
+  const { resultats, resultIsLoading } = props;
+  return (
+    <>
+      {resultIsLoading && <Loading />}
+
+      {!resultIsLoading &&
+        resultats &&
+        resultats.length > 0 &&
+        resultats.map((resultat: Resultat) => (
+          <Card
+            sx={{ minWidth: 275 }}
+            key={`${resultat.code_parametre}-${resultat.date_prelevement}`}
+          >
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {convertDate(resultat.date_prelevement)}
+              </Typography>
+              <Typography variant="h5" component="div">
+                {resultat.libelle_parametre}
+              </Typography>
+              <Typography variant="body2">
+                {resultat.conclusion_conformite_prelevement}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+    </>
+  );
+}
