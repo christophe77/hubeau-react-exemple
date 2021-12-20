7a9a8bfd76a587af96fd85843057e9fd774db55f
christophe bellec <christophe.bellec@ekim.fr>
2021-12-20 16:31:28 +0100

    ajout openstreetmap

--- src/pages/Hydrometrie/index.tsx
+++ src/pages/Hydrometrie/index.tsx
@@ -3,6 +3,8 @@ import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
 import Grid from "@mui/material/Grid";
 import NativeSelect from "@mui/material/NativeSelect";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
 import useHydrometrie from "./useHydrometrie";
 import { convertDateTime } from "../../utils";
 import Loading from "../../common/Loading";
@@ -11,6 +13,10 @@ import PageTitle from "../../common/PageTitle";
 export default function Hydrometrie() {
   const { isLoading, stations, relevesH, relevesQ, getRelevesHydro } =
     useHydrometrie();
+  const position: LatLngExpression =
+    relevesH && relevesH[0] && relevesH[0].latitude
+      ? [relevesH[0].latitude, relevesH[0].longitude]
+      : [0, 0];
   return (
     <>
       <PageTitle path={"/hydrometrie"} />
@@ -42,6 +48,25 @@ export default function Hydrometrie() {
       </FormControl>
       {isLoading && <Loading />}
       <Grid container>
+        <Grid item xs={12}>
+          {!isLoading && relevesH.length > 0 && relevesQ.length > 0 && (
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={position}>
+                <Popup>
+                  {convertDateTime(relevesH[0].date_obs)} <br />
+                  Hauteur (m) : {Number(relevesH[0].resultat_obs) / 10000}
+                  <br /><br />
+                  {convertDateTime(relevesQ[0].date_obs)} <br />
+                  Débit (m³/s) : {relevesQ[0].resultat_obs}
+                </Popup>
+              </Marker>
+            </MapContainer>
+          )}
+        </Grid>
         <Grid item xs={12} sm={6}>
           <h4>Hauteur (m)</h4>
           {!isLoading &&
